f5e552e20b278e61a1aa75d79b06c80d20dbb1cd
Mansi <mansiv19@gmail.com>
2022-08-15 23:33:03 +0530

    Adding the hashing and salting technique for encryption

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -21,9 +21,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
-
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
@@ -39,18 +36,22 @@ app.get("/login", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
 });
 
 app.post("/login", function(req, res) {
@@ -64,15 +65,16 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
-      } else {
-        res.render("login");
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          } else {
+            res.render("login");
+          }
+        });
       }
     }
   });
-
 });
 
 
